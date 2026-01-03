a5dc56ff885f649f43a9e2878aa5b90afc1b8ff1
V-Jagath <jagath9360@gmail.com>
2026-01-03 11:35:20 +0530

    Tasks Are Completed

---
+++ src/Typescript/Interface.tsx
@@ -0,0 +1,25 @@
+export interface taskData {
+    link: string,
+    name: string
+}
+
+export interface userData {
+    userName: string,
+    age: string,
+    email: string,
+    mobile: string,
+    password: string | number,
+
+}
+
+export interface profileData {
+    profileName: string,
+    age: string
+}
+
+export interface AdminData {
+    name: string,
+    age: string,
+    role: string
+}
+
