c5b103ad43f841035894229201da22a33c6e9bbc
V-Jagath <jagath9360@gmail.com>
2026-01-03 22:40:31 +0530

    Move To Top Button Task completed

--- src/Task/Day2Task.tsx
+++ src/Task/Day2Task.tsx
@@ -3,15 +3,20 @@ import { useEffect, useRef, useState } from "react";
 const Day2Task = () => {
     return (
         <>
-            <h1>Day 2 Tasks</h1>
-            <h1>1. Focus input on button click</h1>
-            <FocusInput />
-            <h1>2. Clear Input Using Ref</h1>
-            <ClearInput />
-            <h1>3. AutoSave Input to Local Storage</h1>
-            <AutoSave />
-            <h1>4. Theme change using ref + Tailwind</h1>
-            <ThemeChange />
+            <div className="h-100">
+
+                <h1>Day 2 Tasks</h1>
+                <h1>1. Focus input on button click</h1>
+                <FocusInput />
+                <h1>2. Clear Input Using Ref</h1>
+                <ClearInput />
+                <h1>3. AutoSave Input to Local Storage</h1>
+                <AutoSave />
+                <h1>4. Theme change using ref + Tailwind</h1>
+                <ThemeChange />
+                <h1>5. Scroll to top button</h1>
+                <ScrollTop />
+            </div>
         </>
     )
 }
@@ -57,7 +62,7 @@ export const ClearInput = () => {
 
 // 3. AutoSave Input to Local Storage
 
-const AutoSave = () => {
+export const AutoSave = () => {
 
     const saveRef = useRef<HTMLInputElement>(null)
 
@@ -82,35 +87,54 @@ const AutoSave = () => {
 
 // 4. Theme change using ref + Tailwind
 
-const ThemeChange = () => {
+export const ThemeChange = () => {
 
-    const ThemeRef = useRef<any>(true)
+    const ThemeRef = useRef<HTMLDivElement | null>(null)
+
+    const darkRef = useRef(true)
 
     const [theme, setTheme] = useState<boolean>(true)
 
     const changeTheme = () => {
         setTheme(!theme)
 
-        if (ThemeRef.current) {
-            ThemeRef.current.classList.add("bg-red-600", "text-white")
-            ThemeRef.current = false
-            console.log(ThemeRef.current)
+        if (!ThemeRef.current) return;
+
+        if (darkRef.current) {
+            ThemeRef.current.classList.add("bg-gray-900", "text-white")
+            ThemeRef.current.classList.remove("bg-yellow-300", "text-black")
         } else {
-            ThemeRef.current.classList.remove("bg-red-600", "text-white")
-            // ThemeRef.current.classList.add("p-5", "bg-yellow-600")
-            alert("Else")
+            ThemeRef.current.classList.remove("bg-gray-900", "text-white")
+            ThemeRef.current.classList.add("bg-yellow-300", "text-black")
         }
+        darkRef.current = !darkRef.current
     }
 
     return (<>
         <div className={theme ? "bg-black text-white" : ""}>
             <div>This is a Theme Change using State</div>
-            <button className={theme ? "bg-white text-black px-3 py-1" : "bg-black text-white px-3 py-1"} onClick={changeTheme}>{theme ? "Light" : "Dark"}</button>
+            <button className={theme ? "bg-white text-black px-3 py-1" : "bg-black text-white px-3 py-1 "} onClick={changeTheme}>{theme ? "Light" : "Dark"}</button>
         </div>
 
         <div ref={ThemeRef} >
             <div>This is Theme Change Component Using useRef Hook</div>
             <button onClick={changeTheme}>Change Theme</button>
         </div>
+    </>)
+}
+// 5. Scroll to top button
+export const ScrollTop = () => {
+
+    const GoToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+        // alert("TO goto top")
+    }
+
+
+    return (<>
+        <button onClick={GoToTop}>Go To Top </button>
     </>)
 }
