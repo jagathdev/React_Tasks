5ebcdcabcdd65246a8bfa1a3c8b1dd65ca0e1395
V-Jagath <jagath9360@gmail.com>
2026-01-03 19:07:47 +0530

    Task Completed

--- src/Task/Day2Task.tsx
+++ src/Task/Day2Task.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Day2Task = () => {
     return (
@@ -10,6 +10,8 @@ const Day2Task = () => {
             <ClearInput />
             <h1>3. AutoSave Input to Local Storage</h1>
             <AutoSave />
+            <h1>4. Theme change using ref + Tailwind</h1>
+            <ThemeChange />
         </>
     )
 }
@@ -75,5 +77,40 @@ const AutoSave = () => {
     return (<>
         <input ref={saveRef} type="text" onChange={saveInput} placeholder="Typehere...." />
 
+    </>)
+}
+
+// 4. Theme change using ref + Tailwind
+
+const ThemeChange = () => {
+
+    const ThemeRef = useRef<any>(true)
+
+    const [theme, setTheme] = useState<boolean>(true)
+
+    const changeTheme = () => {
+        setTheme(!theme)
+
+        if (ThemeRef.current) {
+            ThemeRef.current.classList.add("bg-red-600", "text-white")
+            ThemeRef.current = false
+            console.log(ThemeRef.current)
+        } else {
+            ThemeRef.current.classList.remove("bg-red-600", "text-white")
+            // ThemeRef.current.classList.add("p-5", "bg-yellow-600")
+            alert("Else")
+        }
+    }
+
+    return (<>
+        <div className={theme ? "bg-black text-white" : ""}>
+            <div>This is a Theme Change using State</div>
+            <button className={theme ? "bg-white text-black px-3 py-1" : "bg-black text-white px-3 py-1"} onClick={changeTheme}>{theme ? "Light" : "Dark"}</button>
+        </div>
+
+        <div ref={ThemeRef} >
+            <div>This is Theme Change Component Using useRef Hook</div>
+            <button onClick={changeTheme}>Change Theme</button>
+        </div>
     </>)
 }
