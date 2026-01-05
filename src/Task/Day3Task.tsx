e3bd5fc86b72ed918f0fe956e5b4cca3f02433c8
V-Jagath <jagath9360@gmail.com>
2026-01-05 13:04:47 +0530

    Task 3 Completed

--- src/Task/Day3Task.tsx
+++ src/Task/Day3Task.tsx
@@ -1,7 +1,66 @@
+import { useEffect, useState } from "react";
+
 const Day3Task = () => {
     return (<>
-        <h1>Day 3 Task</h1>
+        <div>
+            <h1>DAY 3 – useEffect + Side Effects</h1>
+            <h1>🔧 TASKS</h1>
+
+            <h1>1. Log on component mount</h1>
+            <Mount />
+            <h1>2. Timer with cleanup</h1>
+            <TimerCleanup />
+            <h1>3. Fetch API and render list</h1>
+            <h1>4. Dependency-based effect</h1>
+            <h1>5. Cleanup example</h1>
+            <h1>6. Scroll event listener</h1>
+            <h1>7. Window resize listener</h1>
+            <h1>8. Auto-focus on load</h1>
+            <h1>9. Change title using effect</h1>
+            <h1>10. localStorage sync with effect</h1>
+
+        </div>
     </>)
 };
 
 export default Day3Task;
+
+// 1. Log on component mount
+
+export const Mount = () => {
+
+    useEffect(() => {
+        console.log("Component Mounted")
+    }, [])
+
+    return (<>
+        <h1>Log on component mount</h1>
+    </>)
+}
+
+// 2. Timer with cleanup
+
+export const TimerCleanup = () => {
+
+    const [time, setTime] = useState<number>(0)
+
+
+    useEffect(() => {
+
+        setInterval(() => {
+            setTime(time + 1)
+        }, 1000)
+
+    }, [])
+
+    const CleaupTimer = () => {
+
+    }
+
+    return (<>
+        <div>
+            <h1>Timer</h1>
+            <button onClick={CleaupTimer}>CleanUp</button>
+        </div>
+    </>)
+}
