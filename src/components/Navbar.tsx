455fe34f9c71e13bca1f19b7be5f7a7960b8c6aa
V-Jagath <jagath9360@gmail.com>
2026-01-06 12:06:53 +0530

    Day -5 useContext task completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,8 @@ const Navbar = () => {
     const { theme, themeChange } = useContext(ThemeContext)!;
     return (
         <>
-            <div className="flex justify-between bg-blue-400 p-5">
+            <div className={theme ? "flex justify-between bg-gray-900 p-5" : "flex justify-between bg-gray-400 p-5"}>
+
                 <Link to='/'><h1 className="bg-gray-900 px-3 py-1 text-white rounded font-bold">Logo</h1></Link>
                 <div className="flex gap-10">
                     <Link to='/' className="bg-black px-3 py-1 text-white rounded font-bold">Home</Link>
