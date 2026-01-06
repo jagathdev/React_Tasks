832113b57b3620d845ec20a06cc6989df01bac97
V-Jagath <jagath9360@gmail.com>
2026-01-06 12:32:34 +0530

    Task Completed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,33 @@
+import { useState, type FormEvent } from "react"
 
 const Login = () => {
+
+    const [loginData, setLoginData] = useState<null>(null);
+
+    const getLoginData = (e: any) => {
+        e.preventDefault();
+
+        // setLoginData(e.target.value)
+        console.log(loginData)
+    }
+
+    const getInputData = (e: any) => {
+
+        // const { name, password }: { name: string, password: number | string } = e.target
+
+        // setLoginData({ ...loginData, })
+
+    }
+
     return (
         <div>
-            Its a Login Page
+            <form onSubmit={getLoginData}>
+                <label>Name :</label>
+                <input type="text" onChange={getInputData} name="name" placeholder="Enter User Name" />
+                <label >Password</label>
+                <input type="text" onChange={getInputData} name="password" placeholder="Enter password" />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     )
 }
---
+++ src/usecontext/UserProvide.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const UserProvide = () => {
+
+    // const []
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default UserProvide
